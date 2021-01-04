6ce68de03aca6e8288fc7c4d07ccab873d4224b6
ktleary <kevin@ktleary.com>
2021-01-04 12:13:44 +0800

    add more util tests

--- src/Genstar/__tests__/util.tests.js
+++ src/Genstar/__tests__/util.tests.js
@@ -1,6 +1,7 @@
 import { cleanup } from "@testing-library/react";
 import { remotePath } from "../constants";
 import { getEnv } from "../util/env";
+import { randomInt, randomItems } from "../util/sort";
 
 function overrideWindowHostName({ hostname, href, search }) {
   delete global.window.location;
@@ -23,6 +24,36 @@ test("should return the remotePath", () => {
   });
 
   expect(getEnv()).toEqual(remotePath);
+});
+
+test("it should return 1 random item", () => {
+  const array = ["one", "two", "three"];
+  const items = randomItems(array);
+
+  expect(items.length).toEqual(1);
+});
+
+test("it should return 3 random item", () => {
+  const array = ["one", "two", "three"];
+  const items = randomItems(array, 3);
+
+  expect(items.length).toEqual(3);
+});
+
+test("it should return 2 random item not including existing item", () => {
+  const array = ["one", "two", "three"];
+  const items = randomItems(array, 2, "two");
+
+  expect(items.length).toEqual(2);
+  expect(items.includes("two")).toEqual(false);
+});
+
+it("should return a random number between 1 and 10", () => {
+  const min = 1;
+  const max = 10;
+  const random = randomInt(min, max);
+
+  expect(random < 10 && random > 0).toEqual(true);
 });
 
 afterEach(cleanup);
