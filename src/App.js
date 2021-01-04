5908e3d84b231fa09dc35bb2f6365d80dd888b9f
ktleary <kevin@ktleary.com>
2021-01-05 00:13:10 +0800

    preload images with object & callback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Genstar from "./Genstar/index";
 import { getEnv } from "./Genstar/util/env";
 
 export default function App() {
+  const [images, setImages] = useState({});
   useEffect(() => {
-    function preloadImages() {
+    function preloadImages(cb) {
+      const images = {};
       const path = getEnv();
       let i = 161;
       while (i > 0) {
         const imghld = new Image();
         imghld.src = `${path}/images/${i}.png`;
+        images[imghld.src] = imghld;
         i--;
       }
+      cb(images);
     }
 
-    preloadImages();
+    preloadImages((images) => setImages(images));
   }, []);
 
-  return <Genstar />;
+  return <Genstar images={images} />;
 }
